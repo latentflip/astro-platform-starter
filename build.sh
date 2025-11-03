8cdb0b55bdea2d7ebba670ec3ab1bbe28a44a5da
Philip Roberts <phil@xchart.com>
2025-11-03 15:00:14 +0000

    Add logs to build

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ cp -r nlf .netlify
 sed "s/DEPLOY_ID/${DEPLOY_ID}/g" index.html > dist/index.html
 sed "s/DEPLOY_ID/${DEPLOY_ID}/g" index.js > dist/index.js
 sed "s/DEPLOY_ID/${DEPLOY_ID}/g" lazy.html > dist/lazy.html
+
+ls -al .netlify
+
+cat .netlify/v1/config.json
+cat .netlify/v1/skew-protection.json
