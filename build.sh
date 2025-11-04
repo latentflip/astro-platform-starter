2323517108947a297b70bcdb2e2be62a5422b90c
Philip Roberts <phil@xchart.com>
2025-11-04 16:58:35 +0000

    Add css

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir dist
 sed "s/DEPLOY_ID/${DEPLOY_ID}/g" index.html > dist/index.html
 sed "s/DEPLOY_ID/${DEPLOY_ID}/g" index.js > dist/index.js
 sed "s/DEPLOY_ID/${DEPLOY_ID}/g" lazy.html > "dist/lazy.$DEPLOY_ID.html"
-
+cp index.css dist/index.css
 
 mkdir -p .netlify/v1
 cp nlf/v1/skew-protection.json .netlify/v1/skew-protection.json
